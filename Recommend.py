e325fd86a2880c6243f4f33cdffb33c3072ad6de
Rhys909 <rhyskconnor99@gmail.com>
2019-03-25 23:40:09 +0000

    Continued trialing methods that havenrt worked

--- Recommend.py
+++ Recommend.py
@@ -1,5 +1,10 @@
-hist_file = open("history.txt", "r")
-hist_list = []
-for line in hist_file:
-    print(line)
-hist_file.close()
+def read_history():
+    hist_file = open("history.txt", "r")
+    hist_list = []
+    for line in hist_file:
+        for int in line:
+            hist_list.append()
+    hist_file.close()
+    print(hist_dict)
+
+read_history()
