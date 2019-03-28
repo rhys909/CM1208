7d9d4f008e37d52acbee6ec182b499c7b2698bc0
rhys909 <rhyskconnor99@gmail.com>
2019-03-28 10:07:21 +0000

    Added more

--- Recommend.py
+++ Recommend.py
@@ -1,10 +1,55 @@
+import numpy as np
+from itertools import zip_longest
+
 def read_history():
     hist_file = open("history.txt", "r")
-    hist_list = []
-    for line in hist_file:
-        for int in line:
-            hist_list.append()
+    hist_content = hist_file.read()[6:].split()
     hist_file.close()
-    print(hist_dict)
+    return hist_content
 
-read_history()
+def read_cart():
+    cart_file = open("queries.txt", "r")
+    cart_content = cart_file.readlines()
+    cart_list = [i.strip() for i in cart_content]
+    return cart_list
+
+def calculate_angle():
+    hist = read_history()
+    cart_query = read_cart()
+    temp_list = []
+    count = 0
+    print(cart_query[0])
+    for i in range(len(hist)):
+        t = hist[i]
+        if t == cart_query[0]:
+            count += 1
+        else:
+            temp_list.append(hist[i])
+
+    temp_list = np.array(temp_list, dtype=float)
+    print(angle(temp_list,[2,0,0,0,0,0,0,0]))
+    print(temp_list)
+
+def entries():
+    hist_list = read_history()
+    count = 0
+    for i in hist_list:
+        if i == hist_list[:]:
+            count += 0
+        elif i != hist_list[:]:
+            count += 1
+        else:
+            print("What Happened!")
+    print(count)
+
+def cart_calculator():
+    hist_list = read_history()
+    cart_list = read_cart()
+
+
+hist_list = read_history()
+cart_list = read_cart()
+print(hist_list)
+print(cart_list)
+entries()
+calculate_angle()
